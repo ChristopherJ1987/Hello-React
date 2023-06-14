66f47354b56822838fdf578104f904d5034c8224
Christopher J <chrisbjones1987@yahoo.com>
2023-06-14 14:53:34 -0500

    Added Styling and React Image

--- src/App.js
+++ src/App.js
@@ -6,32 +6,29 @@ function Header() {
     <header>
       <nav className='nav'>
         <img src="/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg" class="App-logo" alt="logo"></img>
-        <ul className='nav-items'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <h1>React Facts</h1>
+        <h2>React Course - Project 1</h2>
       </nav>
     </header>
   )
 }
 function Body() {
   return (
-    <body className='body'>
+    <body className='main'>
       <h1>Fun facts about React</h1>
       <ul>
         <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>He has well over 100K stars on GitHub</li>
         <li>React is maintained by Facebook</li>
+        <li>He has well over 100K stars on GitHub</li>
+        <li>Was originally created by Jordan Walke</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
       <h1>Reasons I'm excited to learn React</h1>
       <ul>
         <li>It's a popular library</li>
         <li>I can easily level up my coding skills</li>
-        <li>I'll be more likely to get a job as a developer</li>
         <li>React applications are fast and reliable</li>
+        <li>I'll be more likely to get a job as a developer</li>
       </ul>
     </body>
   )
